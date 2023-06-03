b5ccdc0df0c8071abc3dd4cae81e5c0210baecaf
ryanvanle <ryanle4321@gmail.com>
2023-06-02 23:49:31 -0700

    I FIXED THE BUG AFTER 7 HOURS

--- server/server/server/settings.py
+++ server/server/server/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +48,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = "server.urls"
@@ -105,6 +107,7 @@ USE_I18N = True
 
 USE_TZ = True
 
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
--- server/server/server/views.py
+++ server/server/server/views.py
@@ -1,5 +1,7 @@
 import requests
 from django.http import HttpResponse
+from PIL import Image
+from io import BytesIO
 
 def get_streetview_image(request):
     if request.method == 'GET':
@@ -9,7 +11,7 @@ def get_streetview_image(request):
         pov = request.GET.get('pov')
         image_width = request.GET.get('image_width')
         image_height = request.GET.get('image_height')
-        api_key = 'AIzaSyBrihFqby1UCOB9U0pMfSHauXWZlFtLfek'  # Replace with your Google Maps API key
+        api_key = 'AIzaSyBrihFqby1UCOB9U0pMfSHauXWZlFtLfek'
 
         if latitude and longitude and angle and pov and image_width and image_height:
             url = f"https://maps.googleapis.com/maps/api/streetview?size={image_width}x{image_height}&location={latitude},{longitude}&heading={angle}&key={api_key}&pov={pov}"
@@ -19,8 +21,19 @@ def get_streetview_image(request):
                 response = requests.get(url)
 
                 if response.status_code == 200:
-                    # Return the image as the response
-                    return HttpResponse(response.content, content_type='image/jpeg')
+                    # Convert the image to PNG format
+                    image = Image.open(BytesIO(response.content))
+                    image = image.convert("RGB")
+
+                    # Create a BytesIO object to store the converted image
+                    image_io = BytesIO()
+
+                    # Save the image as PNG to the BytesIO object
+                    image.save(image_io, format='JPEG')
+                    image_io.seek(0)
+
+                    # Return the image as the response with the appropriate content type
+                    return HttpResponse(image_io, content_type='image/jpeg')
 
                 return HttpResponse("Failed to retrieve image from Google Maps API.", status=500)
 
